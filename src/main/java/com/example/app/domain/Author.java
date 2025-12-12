608d6ec8a51b51f3821fc083159f0f844c5d1883
Otomo <skmnhss0821@gmil.com>
2025-12-12 10:59:23 +0900

    Authorクラスの作成

---
+++ src/main/java/com/example/app/domain/Author.java
@@ -0,0 +1,13 @@
+package com.example.app.domain;
+
+import lombok.Data;
+
+@Data
+public class Author {
+
+	private Integer id;
+	private String name;
+	private String loginId;
+	private String loginPass;
+	
+}
